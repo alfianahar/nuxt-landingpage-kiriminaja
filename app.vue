d58cabe0c94c4e97761ea874ebfacb5a1eadfcd7
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-11-09 00:14:58 +0700

    init and test tailwind

--- app.vue
+++ app.vue
@@ -1,3 +1,5 @@
 <template>
-  <div>Hello World of Nuxt 3</div>
+  <div class="flex items-center justify-center h-screen w-screen">
+    <h1 class="text-3xl font-bold underline">Hello World of Nuxt 3</h1>
+  </div>
 </template>
