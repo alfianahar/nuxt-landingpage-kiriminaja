81ca6847da3350dc081231603b2b2942829123bd
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-11-10 04:06:57 +0700

    init daisy and responsive navbar

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-center h-screen w-screen">
-    <h1 class="text-3xl font-bold underline">Hello World of Nuxt 3</h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div data-theme="mytheme">
+    <Navbar />
+    <slot />
+    <Footer />
+  </div>
+</template>
